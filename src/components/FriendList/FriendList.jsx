2e0f7bf04827dc0f6d2e9eb37fa7febe04b95f1f
nadiahusarova <ngusarova911@gmail.com>
2024-07-18 19:07:35 +0300

    friends profile is done

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,17 +1,16 @@
-import FriendListItem from '../FriendListItem/FriendListItem';
-import s from './FriendList.module.css'
+import FriendListItem from "../FriendListItem/FriendListItem";
+import s from "./FriendList.module.css";
 
 const FriendList = ({ friends }) => {
-    return (
-      <ul >
-        {friends.map(({ avatar, name, isOnline, id }) => (
-          <li  key={id}>
-            <FriendListItem avatar={avatar} name={name} isOnline={isOnline} />
-          </li>
-        ))}
-      </ul>
-    );
-  };
-
+  return (
+    <ul className={s.list}>
+      {friends.map(({ avatar, name, isOnline, id }) => (
+        <li className={s.item} key={id}>
+          <FriendListItem avatar={avatar} name={name} isOnline={isOnline} />
+        </li>
+      ))}
+    </ul>
+  );
+};
 
 export default FriendList;
--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -4,7 +4,7 @@ const FriendListItem = ({ name, avatar, isOnline }) => {
   return (
     <div className={s.wrap}>
       <img className={s.avatar} src={avatar} alt={name} width="48" />
-      <p className={nikname}>{name}</p>
+      <p className={s.nikname}>{name}</p>
       <p className={`${s.status} ${isOnline ? s.online : s.offline}`}>
         {isOnline ? "Online" : "Offline"}
       </p>
